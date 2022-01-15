b8630042d39afc9b390d7aac9bd5506b05a4ca4a
Celine Rayner <celinerayner@gmail.com>
2022-01-15 15:45:20 +0200

    Refactored: EditInfoWindow

---
+++ src/EditInfoWindow.js
@@ -0,0 +1,151 @@
+import { Typography } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { InfoWindow } from '@react-google-maps/api';
+import { useState } from 'react';
+
+
+export default function EditInfoWindow({ focused, handleUpdate, handleArchive, setFocused }) {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [type, setType] = useState("");
+    const [edit, setEdit] = useState(false);
+    const [dialogueOpen, setDialogueOpen] = useState(false);
+
+    const handleEventClose = () => {
+        setFocused(null);
+    }
+
+    const handleChangeName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleChangeDescription = (e) => {
+        setDescription(e.target.value)
+    }
+
+    const handleChangeType = (e) => {
+        setType(e.target.value)
+    }
+
+    const handleDialogueOpen = () => {
+        setDialogueOpen(true);
+    };
+
+    const handleDialogueClose = () => {
+        setDialogueOpen(false);
+    };
+
+    const handleEdit = () => {
+        setName(focused.name);
+        setDescription(focused.description);
+        setType(focused.type);
+        setEdit(true);
+    }
+
+    const handleFinishUpdate = () => {
+        setEdit(false)
+        handleUpdate(focused.key, name, description, type)
+    }
+
+    const handleArchiveButton = () => {
+        setDialogueOpen(false)
+        handleArchive(focused.key)
+    }
+
+    return (
+        <InfoWindow
+            position={focused}
+            onCloseClick={handleEventClose}
+        >
+            <div style={{ textAlign: "left" }}>
+                <Grid item xs={12} p={1}>
+                    {edit ? (<TextField
+                        fullWidth
+                        id="name"
+                        label="Name"
+                        value={name}
+                        onChange={handleChangeName}
+                    />)
+                        : <Typography> Name: {focused.name} </Typography>}
+                </Grid>
+                <Grid item xs={12} p={1}>
+                    {edit ? (<TextField
+                        fullWidth
+                        id="description"
+                        label="Description"
+                        value={description}
+                        onChange={handleChangeDescription}
+                    />)
+                        : <Typography> Description: {focused.description} </Typography>}
+                </Grid>
+                <Grid item xs={12} p={1}>
+                    {edit ?
+                        (<Select
+                            fullWidth
+                            labelId="type"
+                            id="type"
+                            value={type}
+                            onChange={handleChangeType}
+                        >
+                            <MenuItem value={"poi"}>Point of Interest</MenuItem>
+                            <MenuItem value={"hazard"}>Hazard</MenuItem>
+                            <MenuItem value={"report"}>Report</MenuItem>
+                        </Select>)
+                        : <Typography> Type: {focused.type} </Typography>}
+                </Grid>
+                <Grid item xs={12} p={1}>
+                    {!edit && <Button
+                        variant="contained"
+                        onClick={handleEdit}
+                        fullWidth
+                    >
+                        Edit
+                    </Button>}
+                    {edit && <Button
+                        variant="contained"
+                        fullWidth
+                        onClick={handleFinishUpdate}
+                    >
+                        Update
+                    </Button>
+                    }
+                </Grid>
+                <Grid item xs={12} p={1}>
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        onClick={handleDialogueOpen}
+                    >
+                        Archive
+                    </Button>
+                </Grid>
+                <Dialog
+                    open={dialogueOpen}
+                    onClose={handleDialogueClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Are you sure you would like to archive {name}?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleDialogueClose}>No</Button>
+                        <Button onClick={handleArchiveButton} autoFocus>
+                            Yes
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        </InfoWindow>
+    )
+}
--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import { useState, useEffect } from 'react';
-import TextField from '@mui/material/TextField';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-import { Typography } from '@mui/material';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import { useContext } from 'react';
 import { MarkerContext } from './App';
 import EventMarker from './EventMarker';
 import NewEventMarker from './NewEventMarker';
+import EditInfoWindow from './EditInfoWindow';
 
 const defaultCenter = {
     lat: 41.3851, lng: 2.1734
@@ -26,12 +17,7 @@ const defualtZoom = 13;
 
 const Main = () => {
     const [marker, setMarker] = useState(null);
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [type, setType] = useState("");
     const [snackOpen, setSnackOpen] = useState(false);
-    const [edit, setEdit] = useState(false);
-    const [dialogueOpen, setDialogueOpen] = useState(false);
     const [mapCenter, setMapCenter] = useState(defaultCenter);
     const [zoom, setZoom] = useState(defualtZoom);
     const [message, setMessage] = useState("")
@@ -53,26 +39,6 @@ const Main = () => {
 
     const handleMapClick = (e) => {
         setMarker({ lng: e.latLng.lng(), lat: e.latLng.lat() });
-        setName("");
-        setDescription("");
-        setType("");
-    }
-
-
-    const handleEventClose = () => {
-        setFocused(null);
-    }
-
-    const handleChangeName = (e) => {
-        setName(e.target.value)
-    }
-
-    const handleChangeDescription = (e) => {
-        setDescription(e.target.value)
-    }
-
-    const handleChangeType = (e) => {
-        setType(e.target.value)
     }
 
     const addMarker = (name, description, type) => {
@@ -99,14 +65,8 @@ const Main = () => {
         setSnackOpen(false);
     };
 
-    const handleEdit = () => {
-        setName(focused.name);
-        setDescription(focused.description);
-        setType(focused.type);
-        setEdit(true);
-    }
 
-    const handleUpdate = (key) => {
+    const handleUpdate = (key, name, description, type) => {
         console.log(focused.key, key);
         const newMarkers = markers.map((marker) => {
             if (marker.key === key) {
@@ -127,19 +87,9 @@ const Main = () => {
         });
         setMarkers(newMarkers)
         console.log(newMarkers)
-        setEdit(false);
     }
 
-    const handleDialogueOpen = () => {
-        setDialogueOpen(true);
-    };
-
-    const handleDialogueClose = () => {
-        setDialogueOpen(false);
-    };
-
     const handleArchive = (key) => {
-        setDialogueOpen(false);
         const archivedMarkers = markers.map((marker) => {
             if (marker.key === key) {
                 const archived = {
@@ -178,92 +128,7 @@ const Main = () => {
                 {marker && (<NewEventMarker marker={marker} setMarker={setMarker} snackBar={snackBar} addMarker={addMarker} />)}
                 {markers && markers.filter(marker => marker.archived === false).map((m) => <EventMarker m={m} />)}
 
-                {focused && (<InfoWindow
-                    position={focused}
-                    onCloseClick={handleEventClose}
-                >
-                    <div style={{ textAlign: "left" }}>
-                        <Grid item xs={12} p={1}>
-                            {edit ? (<TextField
-                                fullWidth
-                                id="name"
-                                label="Name"
-                                value={name}
-                                onChange={handleChangeName}
-                            />)
-                                : <Typography> Name: {focused.name} </Typography>}
-                        </Grid>
-                        <Grid item xs={12} p={1}>
-                            {edit ? (<TextField
-                                fullWidth
-                                id="description"
-                                label="Description"
-                                value={description}
-                                onChange={handleChangeDescription}
-                            />)
-                                : <Typography> Description: {focused.description} </Typography>}
-                        </Grid>
-                        <Grid item xs={12} p={1}>
-                            {edit ?
-                                (<Select
-                                    fullWidth
-                                    labelId="type"
-                                    id="type"
-                                    value={type}
-                                    onChange={handleChangeType}
-                                >
-                                    <MenuItem value={"poi"}>Point of Interest</MenuItem>
-                                    <MenuItem value={"hazard"}>Hazard</MenuItem>
-                                    <MenuItem value={"report"}>Report</MenuItem>
-                                </Select>)
-                                : <Typography> Type: {focused.type} </Typography>}
-                        </Grid>
-                        <Grid item xs={12} p={1}>
-                            {!edit && <Button
-                                variant="contained"
-                                onClick={handleEdit}
-                                fullWidth
-                            >
-                                Edit
-                            </Button>}
-                            {edit && <Button
-                                variant="contained"
-                                fullWidth
-                                onClick={() => handleUpdate(focused.key)}
-                            >
-                                Update
-                            </Button>
-                            }
-                        </Grid>
-                        <Grid item xs={12} p={1}>
-                            <Button
-                                variant="contained"
-                                fullWidth
-                                onClick={handleDialogueOpen}
-                            >
-                                Archive
-                            </Button>
-                        </Grid>
-                        <Dialog
-                            open={dialogueOpen}
-                            onClose={handleDialogueClose}
-                            aria-labelledby="alert-dialog-title"
-                            aria-describedby="alert-dialog-description"
-                        >
-                            <DialogContent>
-                                <DialogContentText id="alert-dialog-description">
-                                    Are you sure you would like to archive {name}?
-                                </DialogContentText>
-                            </DialogContent>
-                            <DialogActions>
-                                <Button onClick={handleDialogueClose}>No</Button>
-                                <Button onClick={() => handleArchive(focused.key)} autoFocus>
-                                    Yes
-                                </Button>
-                            </DialogActions>
-                        </Dialog>
-                    </div>
-                </InfoWindow>)}
+                {focused && (<EditInfoWindow focused={focused} handleUpdate={handleUpdate} handleArchive={handleArchive} setFocused={setFocused} />)}
                 <Snackbar open={snackOpen} autoHideDuration={5000} onClose={handleClose}>
                     <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
                         {message}
